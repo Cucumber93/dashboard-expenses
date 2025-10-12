80ae41cb9b56cc7068efdfb206923a81cd6db4ce
CucumberK <ruangthiwa.t@kkumail.com>
2025-10-12 12:42:08 +0700

    feature: add balance service api

---
+++ src/services/balance.service.ts
@@ -0,0 +1,16 @@
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+export const BalanceService = {
+    getBalance: async()=>{
+        try{
+            const response = await fetch(`${BASE_URL}/balance`);
+            if(!response.ok){
+                throw new Error("Failed to fetch balance");
+            }
+            return await response.json();
+        }catch(error){
+            console.error("Error fetching balance:", error);
+            throw error;
+        }
+    }
+}
