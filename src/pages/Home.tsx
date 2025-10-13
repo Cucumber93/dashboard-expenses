75de1240031d10f39dcdab99092ea000a45ee539
CucumberK <ruangthiwa.t@kkumail.com>
2025-10-13 22:25:45 +0700

    debugs: console.log to see user line

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,31 @@
 
 // import './App.css'
+import { useEffect, useState } from "react"
 import ChartCard from "../pages/ChartCard"
 import { OverviewCard } from "../pages/OverviewCard"
 import Topbar from "../pages/Topbar"
+import { useNavigate } from "react-router-dom"
 
 
 function App() {
-  
+  const [user, setUser] = useState<any>(null)
+  const navigate = useNavigate()
+  useEffect(() => {
+      const userData = localStorage.getItem("user")
+      if (userData) {
+        setUser(JSON.parse(userData))
+      } else {
+        navigate("/login")
+      }
+    }, [navigate])
+
+    useEffect(()=>{
+      console.log('huhhhhhhhhhhhhh')
+      console.log('user: ',user)
+    },[user])
   return (
-    <>
+    <>ddd
+    {user}
       <Topbar/>
       <OverviewCard/>
       <ChartCard/>
