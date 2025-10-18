c4155286320e1db6371dba89f85791b5632c34f1
CucumberK <ruangthiwa.t@kkumail.com>
2025-10-18 17:35:32 +0700

    bugs: comment useEffect on home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,24 +8,23 @@ import { useNavigate } from "react-router-dom"
 
 
 function App() {
-  const [user, setUser] = useState<any>(null)
-  const navigate = useNavigate()
-  useEffect(() => {
-      const userData = localStorage.getItem("user")
-      if (userData) {
-        setUser(JSON.parse(userData))
-      } else {
-        navigate("/login")
-      }
-    }, [navigate])
+  // const [user, setUser] = useState<any>(null)
+  // const navigate = useNavigate()
+  // useEffect(() => {
+  //     const userData = localStorage.getItem("user")
+  //     if (userData) {
+  //       setUser(JSON.parse(userData))
+  //     } else {
+  //       navigate("/login")
+  //     }
+  //   }, [navigate])
 
-    useEffect(()=>{
-      console.log('huhhhhhhhhhhhhh')
-      console.log('user: ',user)
-    },[user])
+  //   useEffect(()=>{
+  //     console.log('huhhhhhhhhhhhhh')
+  //     console.log('user: ',user)
+  //   },[user])
   return (
     <>ddd
-    {user}
       <Topbar/>
       <OverviewCard/>
       <ChartCard/>
