1d6b0381402c73d309b983cb1fe1c4572dd3a19c
CucumberK <ruangthiwa.t@kkumail.com>
2025-10-25 21:37:48 +0700

    fix: path on loginLine

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -5,7 +5,7 @@ const BASE_URL = import.meta.env.VITE_BASE_URL;
 export const AuthService = {
   loginLine: async(data:any)=>{
     try{
-        const response = await axios.post(`${BASE_URL}/login-line`,data)
+        const response = await axios.post(`${BASE_URL}/auth/login-line`,data)
         return response.data
     }catch(err){    
         console.log(err)
