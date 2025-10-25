abab6aa5282797d71fdf08c32e793a71e85f3d3b
CucumberK <ruangthiwa.t@kkumail.com>
2025-10-25 21:28:26 +0700

    feature: handleLogin

--- src/pages/auth/Line.tsx
+++ src/pages/auth/Line.tsx
@@ -1,6 +1,6 @@
 import liff from '@line/liff'
 import { useEffect } from 'react'
-
+import {AuthService} from '../../services/auth.service'
 const Line = () => {
       useEffect(()=>{
         liff.init({liffId:'2008277464-bBvaglGD'})
@@ -12,10 +12,12 @@ const Line = () => {
 
       const handleLogin = async()=>{
         try{
-            const profile = await liff.getProfile()
+            // const profile = await liff.getProfile()
             const token = await liff.getAccessToken()
-            console.log('profile: ',profile)
-            console.log('token: ', token)
+            await AuthService.loginLine(token)
+            .then(res=>{
+                console.log(res)
+            }).catch(err=>console.log(err))
         }catch(err){
             console.log(err)
         }
---
+++ src/services/auth.service.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+export const AuthService = {
+  loginLine: async(data:any)=>{
+    try{
+        const response = await axios.post(`${BASE_URL}/login-line`,data)
+        return response.data
+    }catch(err){    
+        console.log(err)
+    }
+  }
+};
