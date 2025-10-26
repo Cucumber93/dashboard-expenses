d7eef99073f86c139755c78d06f339835e5bba7a
CucumberK <ruangthiwa.t@kkumail.com>
2025-10-26 23:39:31 +0700

    fis: change to my dashboard

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,42 +1,15 @@
 
-// import './App.css'
-// import { useEffect, useState } from "react"
-// import ChartCard from "../pages/ChartCard"
-// import { OverviewCard } from "../pages/OverviewCard"
-// import Topbar from "../pages/Topbar"
-// import { useNavigate } from "react-router-dom"
+import ChartCard from "./ChartCard";
+import { OverviewCard } from "./OverviewCard";
+import Topbar from "./Topbar";
 
 
 function App() {
-  // const [user, setUser] = useState<any>(null)
-  // const navigate = useNavigate()
-  // useEffect(() => {
-  //     const userData = localStorage.getItem("user")
-  //     if (userData) {
-  //       setUser(JSON.parse(userData))
-  //     } else {
-  //       navigate("/login")
-  //     }
-  //   }, [navigate])
-
-  //   useEffect(()=>{
-  //     console.log('huhhhhhhhhhhhhh')
-  //     console.log('user: ',user)
-  //   },[user])
-  const handleLogout = () => {
-    localStorage.removeItem("access_token");
-    window.location.href = "/login";
-  };
   return (
     <>
-      {/* <Topbar/>
+      <Topbar/>
       <OverviewCard/>
-      <ChartCard/> */}
-      <div style={{ padding: 40 }}>
-      <h1>ยินดีต้อนรับ 🎉</h1>
-      <p>เข้าสู่ระบบด้วย LINE สำเร็จแล้ว</p>
-      <button onClick={handleLogout}>ออกจากระบบ</button>
-    </div>
+      <ChartCard/>
     </>
   )
 }
