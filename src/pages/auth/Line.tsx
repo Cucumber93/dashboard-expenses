b19e0b55037fc6ff155fee51bab7c174e0f76bb8
CucumberK <ruangthiwa.t@kkumail.com>
2025-10-26 16:40:44 +0700

    debugs: checkk token on cookie

--- src/pages/auth/Line.tsx
+++ src/pages/auth/Line.tsx
@@ -1,47 +1,46 @@
-import liff from '@line/liff'
-import { useEffect } from 'react'
-import {AuthService} from '../../services/auth.service'
-import type { IProfile } from '../../interface/line'
+import liff from "@line/liff";
+import { useEffect } from "react";
+import { AuthService } from "../../services/auth.service";
+import type { IProfile } from "../../interface/line";
 const Line = () => {
+  useEffect(() => {
+    handleGetProfile();
+  });
+  useEffect(() => {
+    liff.init({ liffId: "2008277464-bBvaglGD" }).then(() => {
+      //code
+      handleLogin();
+    });
+  }, []);
 
-      useEffect(()=>{
-        liff.init({liffId:'2008277464-bBvaglGD'})
-        .then(()=>{
-            //code
-            handleLogin()
-            handleGetProfile()
+  const handleGetProfile = async () => {
+    try {
+      const token = await AuthService.getTokenCookie();
+      console.log("token from getProfileToken jaa: ", token);
+      return token;
+    } catch (error) {
+      console.log("Can not get Profile token: ", error);
+    }
+  };
+
+  const handleLogin = async () => {
+    try {
+      const profile = await liff.getProfile();
+      const token = await liff.getIDToken();
+      const accessToken = await liff.getAccessToken();
+      console.log("profile: ", profile);
+      console.log("token", token);
+      console.log("access token: ", accessToken);
+      await AuthService.loginLine(profile as IProfile)
+        .then((res) => {
+          console.log(res);
         })
-      },[])
+        .catch((err) => console.log(err));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return <div>Line</div>;
+};
 
-      const handleGetProfile = async()=>{
-        try{
-          const token = await AuthService.getTokenCookie()
-          console.log('token from getProfileToken jaa: ', token)
-          return token
-        }catch(error){
-          console.log('Can not get Profile token: ',error)
-        }
-      }
-
-      const handleLogin = async()=>{
-        try{
-            const profile = await liff.getProfile()
-            const token = await liff.getIDToken()
-            const accessToken= await liff.getAccessToken()
-            console.log('profile: ',profile)
-            console.log('token', token)
-            console.log('access token: ', accessToken)
-            await AuthService.loginLine(profile as IProfile)
-            .then(res=>{
-                console.log(res)
-            }).catch(err=>console.log(err))
-        }catch(err){
-            console.log(err)
-        }
-      }
-  return (
-    <div>Line</div>
-  )
-}
-
-export default Line
+export default Line;
