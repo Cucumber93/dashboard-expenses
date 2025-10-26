d91f5b54f041fb9fc323604971682cfa59ff8a3d
CucumberK <ruangthiwa.t@kkumail.com>
2025-10-26 14:06:06 +0700

    feature: handle cookie

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -3,9 +3,18 @@ import axios from "axios";
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const AuthService = {
-  loginLine: async(data:any)=>{
+  loginLine: async(profile:any)=>{
     try{
-        const response = await axios.post(`${BASE_URL}/auth/login-line`,data)
+        const response = await axios.post(`${BASE_URL}/auth/login-line`,{
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include", // ✅ ให้ browser ส่ง cookie
+        body: JSON.stringify({
+          userId: profile.userId,
+          displayName: profile.displayName,
+          pictureUrl: profile.pictureUrl,
+        }),
+      })
         return response.data
     }catch(err){    
         console.log(err)
