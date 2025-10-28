5b863f855a99613c8d7c32a16f670b5a22eb837a
CucumberK <ruangthiwa.t@kkumail.com>
2025-10-28 23:12:26 +0700

    fix: chage get to post on getBalance

--- src/services/balance.service.ts
+++ src/services/balance.service.ts
@@ -3,10 +3,10 @@ import axios from "axios";
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const BalanceService = {
-  getBalance: async (userId:string) => {
+  getBalance: async (userId: string) => {
     try {
-      const response = await axios.get(`${BASE_URL}/balance`, {
-        params: { userId }, // ✅ axios จะ auto แปลงเป็น ?userId=...
+      const response = await axios.post(`${BASE_URL}/balance`, {
+        userId, // ✅ ส่งใน body เหมือนใน Postman
       });
       return response.data;
     } catch (error) {
