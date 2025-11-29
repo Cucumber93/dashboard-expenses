42716a43b4dd2e81b013c3f2a0dc856cf9d7b031
CucumberK <ruangthiwa.t@kkumail.com>
2025-11-29 22:50:25 +0700

    debugs

--- src/pages/auth/Line.tsx
+++ src/pages/auth/Line.tsx
@@ -1,8 +1,8 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import liff from "@line/liff";
-// import { AuthService } from "../../services/auth.service";
-// import type { IProfile } from "../../interface/line";
+import { AuthService } from "../../services/auth.service";
+import type { IProfile } from "../../interface/line";
 
 export default function Line() {
   const navigate = useNavigate();
@@ -15,17 +15,17 @@ export default function Line() {
           withLoginOnExternalBrowser: true, // ✅ ต้องใส่ในหน้านี้!
         });
 
-        // if (!liff.isLoggedIn()) {
-        //   liff.login({ redirectUri: window.location.href });
-        //   return;
-        // }
+        if (!liff.isLoggedIn()) {
+          liff.login({ redirectUri: window.location.href });
+          return;
+        }
         console.log('is logged in: ', liff.isLoggedIn())
         const profile = await liff.getProfile();
         console.log('profile: ',profile)
-        // const res = await AuthService.loginLine(profile as IProfile);
-        // console.log("Backend response:", res);
+        const res = await AuthService.loginLine(profile as IProfile);
+        console.log("Backend response:", res);
 
-        // navigate("/");
+        navigate("/");
       } catch (err) {
         console.error("LIFF init error:", err);
       }
