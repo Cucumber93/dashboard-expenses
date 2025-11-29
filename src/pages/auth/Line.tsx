9e66f57191e49ee3b09677193f8c9f15e801924b
CucumberK <ruangthiwa.t@kkumail.com>
2025-11-29 22:44:58 +0700

    debug: login

--- src/pages/auth/Line.tsx
+++ src/pages/auth/Line.tsx
@@ -7,70 +7,32 @@ import type { IProfile } from "../../interface/line";
 export default function Line() {
   const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   const initLiff = async () => {
-  //     try {
-  //       await liff.init({
-  //         liffId: "2008277464-bBvaglGD",
-  //         withLoginOnExternalBrowser: true, // ✅ ต้องใส่ในหน้านี้!
-  //       });
+  useEffect(() => {
+    const initLiff = async () => {
+      try {
+        await liff.init({
+          liffId: "2008277464-bBvaglGD",
+          withLoginOnExternalBrowser: true, // ✅ ต้องใส่ในหน้านี้!
+        });
 
-  //       if (!liff.isLoggedIn()) {
-  //         liff.login({ redirectUri: window.location.href });
-  //         return;
-  //       }
+        if (!liff.isLoggedIn()) {
+          liff.login({ redirectUri: window.location.href });
+          return;
+        }
 
-  //       const profile = await liff.getProfile();
-  //       const res = await AuthService.loginLine(profile as IProfile);
+        const profile = await liff.getProfile();
+        console.log('profile: ',profile)
+        const res = await AuthService.loginLine(profile as IProfile);
+        console.log("Backend response:", res);
 
-  //       console.log("Backend response:", res);
+        navigate("/");
+      } catch (err) {
+        console.error("LIFF init error:", err);
+      }
+    };
 
-  //       // ❗ เช็คว่ามี token ไหม
-  //       if (!res?.token) {
-  //         console.error("❌ Token not returned from backend");
-  //         return;
-  //       }
-
-  //       // ❗ รอให้ localStorage เขียน token เสร็จ
-  //       await new Promise((resolve) => setTimeout(resolve, 200));
-
-  //       console.log(
-  //         "TOKEN IN LOCALSTORAGE:",
-  //         localStorage.getItem("auth_token")
-  //       );
-
-  //       navigate("/");
-  //     } catch (err) {
-  //       console.error("LIFF init error:", err);
-  //     }
-  //   };
-
-  //   initLiff();
-  // }, [navigate]);
-
-  useEffect(()=>{
-    console.log('test...')
-    liff.init({liffId:'2008277464-bBvaglGD'})
-    .then(()=>{
-      handleLogin()
-    })
-  },[])
-
-  const handleLogin = async()=>{
-    try{
-      const profile = await liff.getProfile()
-      console.log('profile: ',profile)
-      await AuthService.loginLine(profile as IProfile)
-      .then(res=>{
-        console.log('res: ',res)
-        localStorage.setItem('auth_token',res.data.token)
-        navigate('/')
-      }).catch(err=>console.log(err))
-      
-    }catch(err){
-      console.log(err)
-    }
-  }
+    initLiff();
+  }, [navigate]);
 
   return (
     <div style={{ textAlign: "center", marginTop: 100 }}>
--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -6,22 +6,12 @@ export default function Login() {
     liff.init({ liffId: "2008277464-bBvaglGD" });
   }, []);
 
-  // const handleLogin = () => {
-  //   if (!liff.isLoggedIn()) {
-  //     // เริ่ม login และ redirect ไปหน้า /line หลังสำเร็จ
-  //     liff.login({ redirectUri: window.location.origin + "/line" });
-  //   }
-  // };
-
   const handleLogin = () => {
-    console.log('dddddd')
-    try{
-      liff.login()
-    }catch(err){
-      console.log(err)
+    if (!liff.isLoggedIn()) {
+      // เริ่ม login และ redirect ไปหน้า /line หลังสำเร็จ
+      liff.login({ redirectUri: window.location.origin + "/line" });
     }
   };
-
 
   return (
     <div  className="pr-5 pl-5 p-2 text-[#ffffff]">
